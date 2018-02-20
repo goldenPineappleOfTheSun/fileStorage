6eaca5018de1c94ad1afe94729e7baa8815d029c
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2018-02-20 20:17:39 +0500

    Add DBHelper

---
+++ FileStorage/Goldenpineappleofthesun.Web/DBHelper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Goldenpineappleofthesun.Database.Models;
+using Goldenpineappleofthesun.Database.Repositories;
+
+namespace Goldenpineappleofthesun.Web
+{
+    public class DBHelper
+    {
+        private static NHUserRepository Users = new NHUserRepository();
+        private static NHDocumentRepository Documents = new NHDocumentRepository();
+        private static NHRoleRepository Roles = new NHRoleRepository();
+
+        public DBHelper()
+        {
+
+        }
+
+        #region User
+
+        public static UserItem GetUserByLogin(string login)
+        {
+            return Users.GetByLogin(login);
+        }
+
+        /// <summary>
+        /// Добавить пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        public static void AddUser(string login, string pass, string name, RoleItem role)
+        {
+            var user = new UserItem();
+            user.Id = 0;
+            user.Login = login;
+            user.Password = pass;
+            user.Name = name;
+            user.Role = role;
+
+            AddUser(user);
+        }
+        
+        /// <summary>
+        /// Добавить пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        public static void AddUser(string login, string pass, string name, string role)
+        {
+            // TODO: Check
+            var user = new UserItem();
+            user.Id = 0;
+            user.Login = login;
+            user.Password = pass;
+            user.Name = name;
+            user.Role = DBHelper.GetRoleByName(role);
+
+            AddUser(user);
+        }
+
+        /// <summary>
+        /// Добавить пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        public static void AddUser(UserItem user)
+        {
+            Users.Save(user);
+        }
+
+        #endregion
+
+        #region Document
+
+        /// <summary>
+        /// Добавить пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        public static void AddDocument(string name, string path, UserItem user)
+        {
+            var doc = new DocumentItem();
+            doc.Id = 0;
+            doc.Name = name;
+            doc.FileName = path;
+            doc.Author = user;
+            doc.CreationDate = DateTime.Now;
+
+            AddDocument(doc);
+        }
+
+        /// <summary>
+        /// Добавить пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        public static void AddDocument(string name, string path, string user)
+        {
+            var doc = new DocumentItem();
+            doc.Id = 0;
+            doc.Name = name;
+            doc.FileName = path;
+            doc.Author = user;
+
+            AddDocument(doc);
+        }
+
+        /// <summary>
+        /// Добавить пользователя
+        /// </summary>
+        /// <param name="doc"></param>
+        public static void AddDocument(DocumentItem doc)
+        {
+            Documents.Save(doc);
+        }
+
+        #endregion
+
+        #region Document
+
+        public static RoleItem GetRoleByName(string name)
+        {
+            return Roles.GetByName(name);
+        }
+
+        #endregion
+    }
+}
---
+++ FileStorage/Goldenpineappleofthesun.Web/Models/RegistrationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Goldenpineappleofthesun.Web.Models
+{
+    public class RegistrationModel
+    {
+        [Display(Name = "Логин")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Введите логин")]
+        public string Login { get; set; }
+
+        [Display(Name = "Пароль")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Имя")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Введите имя")]
+        public string Name { get; set; }
+    }
+}
